280cd6c1d062389e3987d9cdd9ced5f920c0494b
m1ckey66063637-cloud <m1ckey66063637@gmail.com>
2025-09-01 13:02:07 +0200

    Update git_exercise.py

--- challenge_day3/git_exercise.py
+++ challenge_day3/git_exercise.py
@@ -1,1 +1,10 @@
+from my_name import get_name as gaomingze_name
+from suijiaying import get_name as suijiaying_name
+
+def team_members():
+    print("We are the late group team. Our members include：")
+    print(gaomingze_name())
+    print(suijiaying_name())
+
+team_members()
 
