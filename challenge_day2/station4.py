fba344c5932a3fa810c7c17af2a1e8d4e97a59ad
m1ckey66063637-cloud <m1ckey66063637@gmail.com>
2025-09-02 16:22:23 +0200

    Update station4.py

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -1,7 +1,8 @@
 def solution_station_4(sample_input: int, real_input: int) -> bool:
-    
-    try:
-        return (real_input % sample_input) == 0
-    except ZeroDivisionError:
-        # If sample_input is zero, treat as not divisible
+    from sympy import isprime
+    if sample_input == 0:
         return False
+    if real_input % sample_input != 0:
+        return False
+    return isprime(real_input // sample_input)
+
