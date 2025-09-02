906935d78630be2a5b483cd4c62af68818c08471
m1ckey66063637-cloud <m1ckey66063637@gmail.com>
2025-09-02 16:29:32 +0200

    Update station5.py

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -1,5 +1,6 @@
-def station5(name1: str, name2: str) -> int:
+def solution_station_5(name1: str, name2: str) -> int:
     set1 = set(name1.lower().replace(" ", ""))
     set2 = set(name2.lower().replace(" ", ""))
     return len(set1 & set2)
+
 
