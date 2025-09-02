867520b5d01ee73f786a1a3401ca7b1210550acb
m1ckey66063637-cloud <m1ckey66063637@gmail.com>
2025-09-02 16:43:40 +0200

    Update station5.py

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -1,6 +1,33 @@
-def solution_station_5(name1: str, name2: str) -> int:
-    set1 = set(name1.lower().replace(" ", ""))
-    set2 = set(name2.lower().replace(" ", ""))
-    return len(set1 & set2)
+def solution_station_5(name: str) -> int:
+    learning_teams = {
+        1: [
+            "Daeho", "David", "Kaisa", "Oliver", "Sara", "Dan", "Ivar", "Lotte", "Riya", "Vassil",
+            "Twan", "Ester", "Karolina", "Lena", "Margarita", "Anna", "Kien", "Klaudia", "Maliah", "Todd"
+        ],
+        2: [
+            "Oumaima", "Mathilde", "Marie", "Anita", "Ziyan", "Bernardo", "Eleanor", "Lorijn", "Maria",
+            "Younes", "Yvan", "Henning", "Liangyu", "Maciej", "Toprak", "Chris", "GengXin", "Mingze", "Phoebe"
+        ],
+        3: [
+            "Betija", "Haider", "Kacper", "Sophie", "Amir", "Baltasar", "Isar", "Jelle", "Nicolas", "David",
+            "Ipek", "Juan", "Marfa", "Maria", "Alissa", "Leopoldo", "Mies", "Jiaying", "Kaixin", "Mai", "Sem", "Tibbe"
+        ],
+        4: [
+            "Justus", "Julia", "Philip", "Uli", "Vanessa", "Anna", "Ekaterina", "Thessa", "Tongfei", "Yang",
+            "Benedikt", "Jan", "Nadee", "Osjah", "Tim", "Eliana", "Joana", "Peilin", "Pija", "Wenhao"
+        ],
+        5: [
+            "Afua", "Cristina", "Greta", "Jace", "Laura", "Anna", "Bassant", "Ivan", "Juriaan", "Kiavash"
+        ],
+        6: [
+            "Keitaro", "Nohemi", "Norina", "Yifan", "Yinan", "Luo", "Nikola", "Olesya", "Sophie", "Tom"
+        ]
+    }
 
+    key = name.strip().casefold()
 
+    for team_number, members in learning_teams.items():
+        if any(key == m.casefold() for m in members):
+            return team_number
+        
+    return -1
